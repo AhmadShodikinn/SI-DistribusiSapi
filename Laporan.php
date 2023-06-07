36dde6192bd650736f1ac57887ab7a814b22d969
AhmadShodikinn <dikiahmad05@gmail.com>
2023-06-07 13:25:37 +0700

    add print, filter pembyaran

--- Laporan.php
+++ Laporan.php
@@ -1,63 +1,86 @@
-<?php 
-    $title = "Laporan Pembayaran";
-    include 'sidebarnav.php';
-    include_once 'config.php';
-    $id = $_SESSION['id_petugas'];
+<?php
+$title = "Laporan Pembayaran";
+include 'sidebarnav.php';
+include_once 'config.php';
+$id = $_SESSION['id_petugas'];
 ?>
 
 <div class="page-wrapper">
-            <div class="container-fluid">
-                <div class="page-breadcrumb">
-                    <div class="row align-items-center">
-                        <div class="align-self-center">
-                            <h3 class="page-title mb-0 p-0">Laporan Pembayaran Peternak</h3s>
-                        </div>
+    <div class="container-fluid">
+        <div class="page-breadcrumb">
+            <div class="row align-align-items-center">
+                <div class="align-self-center">
+                    <h3 class="page-title mb-0 p-0">Laporan Pembayaran Peternak</h3s>
+                </div>
 
-                    <div class="container mt-4">
-                        <form action="Laporan.php" method="GET">
-                            <div class="form-row">
-                                <div class="form-group col-auto">
-                                    <label class="col-form-label">Periode Pembayaran</label>
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <input id="date_start" name="date_start" type="date" class="form-control">
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <input id="date_end" name="date_end" type="date" class="form-control">
-                                </div>
-                                <div class="form-group col-md-3">
-                                    <button class="btn btn-primary" type="submit">Cari</button>
-                                </div>
+                <div class="container mt-4 d-flex  gap-2">
+                    <div class="d-flex gap-3 w-100">
+                        <form action="Laporan.php" method="GET" class="d-flex gap-2 w-100">
+                            <div class="form-group">
+                                <label class="col-form-label">Periode Pembayaran</label>
+                            </div>
+                            <div class="form-group">
+                                <input id="date_start" name="date_start" type="date" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input id="date_end" name="date_end" type="date" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <select name="periode" class="form-control">
+                                    <option value="">Periode</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <button name="cari" class="btn btn-primary" type="submit">Cari</button>
                             </div>
                         </form>
+                        <form action="./export/LaporanPembayaran.php" method="post" class="d-flex align-items-start w-80 ">
+                            <?php if (isset($_GET['date_start']) || isset($_GET['date_end']) || isset($_GET['periode'])) : ?>
+                                <div class="row">
+                                    <div class="form-group col-md-2">
+                                        <input type="hidden" name="date_start" value="<?= $_GET['date_start'] ?>">
+                                        <input type="hidden" name="date_end" value="<?= $_GET['date_end'] ?>">
+                                        <input type="hidden" name="periode" value="<?= $_GET['periode'] ?>">
+                                        <!-- <a href="./export/LaporanPembayaran.php">Print</a> -->
+                                    </div>
+                                </div>
+                            <?php endif ?>
+                            <button name="print" class="btn btn-primary" type="submit">Print</button>
+                        </form>
                     </div>
-                    
 
 
-                    <!-- table  -->
-                    <div class="my-4">
-                        <table id="example" class="table table-striped" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Nama petugas</th>
-                                    <th>Nama peternak</th>
-                                    <th>tanggal pembayaran</th>
-                                    <th>Periode pembayaran</th>
-                                    <th>Total yang dibayarkan</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            <?php 
-                                if(isset($_GET['date_start']) && isset($_GET['date_end'])){
-                                    $data = mysqli_query($conn, "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran BETWEEN '".$_GET['date_start']."' and '".$_GET['date_end']."'");
-                                }else{
+                </div>
+
+
+                <!-- table  -->
+                <div class="my-4">
+                    <table id="example" class="table table-striped" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama petugas</th>
+                                <th>Nama peternak</th>
+                                <th>tanggal pembayaran</th>
+                                <th>Periode pembayaran</th>
+                                <th>Total yang dibayarkan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            if (isset($_GET['date_start']) && isset($_GET['date_end']) && isset($_GET['periode'])) {
+                                $sql = "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran BETWEEN '" . $_GET['date_start'] . "' and '" . $_GET['date_end'] . "' AND periode = '" . $_GET['periode'] . "'";
+                                $data = mysqli_query($conn, $sql);
+                            } else {
                                 //default tanpa filter
-                                    $data = mysqli_query($conn, "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran");	
-                                }
-
-                                $nomor = 1;
-                                while($dataPembayaran = mysqli_fetch_array($data)){ ?>
+                                $data = mysqli_query($conn, "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran");
+                            }
+                            $_SESSION['dataPembayaran'] = $data;
+                            $nomor = 1;
+                            while ($dataPembayaran = mysqli_fetch_array($data)) { ?>
                                 <tr>
                                     <td><?= $nomor++ ?></td>
                                     <td><?= $dataPembayaran['nama'] ?></td>
@@ -65,64 +88,65 @@
                                     <td><?= $dataPembayaran['tanggal_pembayaran'] ?></td>
                                     <td><?= $dataPembayaran['periode'] ?></td>
                                     <td><?= $dataPembayaran['harga_total'] ?></td>
-                                </tr>   
-                                
-                                <?php
-                            }?>
-                            </tbody>
-                        </table>
-                    </div>
+                                </tr>
 
-                    </div>
-                </div>        
+                            <?php
+                            } ?>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
         </div>
+    </div>
 
-        <?php require_once('footer.php') ?>
+    <?php require_once('footer.php') ?>
 
 
 
     <script>
         $(document).ready(() => {
-        $('#example').DataTable();
+            $('#example').DataTable();
 
-        //logic set disable before insert first date
-        document.getElementById('date_end').disabled = true;
-        var dis = document.getElementById('date_start');
-        dis.onchange = () => {
-            if(dis.value == " " ){
-                document.getElementById('date_end').disabled = true;
+            //logic set disable before insert first date
+            document.getElementById('date_end').disabled = true;
+            var dis = document.getElementById('date_start');
+            dis.onchange = () => {
+                if (dis.value == " ") {
+                    document.getElementById('date_end').disabled = true;
+                } else
+                    document.getElementById('date_end').disabled = false;
             }
-            else
-            document.getElementById('date_end').disabled = false;
-        }
 
 
-        //logic date
-        const month = new Date().getMonth();
-        const year = new Date().getFullYear();
+            //logic date
+            const month = new Date().getMonth();
+            const year = new Date().getFullYear();
 
-        const dateTrailing = (str) => str.toString().padStart(2,'0')
+            const dateTrailing = (str) => str.toString().padStart(2, '0')
 
-        const init = () => {
-            $('#date_start').on('change', function() {
-                const val = $(this).val()
-                $('#date_end').attr({
-                    min: val
+            const init = () => {
+                $('#date_start').on('change', function() {
+                    const val = $(this).val()
+                    $('#date_end').attr({
+                        min: val
+                    })
                 })
-            })
 
-            const min = val => {
-                const date = new Date(val)
-                const month = date.getMonth()
-                const year = date.getFullYear()
-                return `${year}-${dateTrailing(month + 1)}-10`
+                const min = val => {
+                    const date = new Date(val)
+                    const month = date.getMonth()
+                    const year = date.getFullYear()
+                    return `${year}-${dateTrailing(month + 1)}-10`
+                }
+
+                // $('#date_start').attr({max})
+                $('#date_end').attr({
+                    min
+                })
             }
-            
-            // $('#date_start').attr({max})
-            $('#date_end').attr({min})
-        }
 
-        init()
+            init()
 
 
         })
---
+++ export/LaporanPembayaran.php
@@ -0,0 +1,124 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/SI-DISTRIBUSISAPI/vendor/dompdf/autoload.inc.php';
+
+use Dompdf\Dompdf;
+
+ob_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/SI-DISTRIBUSISAPI/config.php';
+
+if (isset($_POST['print'])) {
+    $date_start = $_POST['date_start'];
+    $date_end = $_POST['date_end'];
+    $periode = $_POST['periode'];
+};
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Laporan</title>
+</head>
+<style media="print">
+ @page {
+  size: auto;
+  margin: 0;
+       }
+</style>
+
+<style>
+        #signature {
+            width: 100%;
+            display: flex;
+            justify-content: end;
+            margin-left: auto;
+            margin-right: 20px;
+            /* text-align: right;
+            position: absolute;
+            bottom: 50px;
+            right: 0; */
+        }
+
+        #signature .details {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            gap: 40px;
+        }
+
+        #signature .details .identity {
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            justify-content: space-between;
+        }
+
+        #signature .details .identity p {
+            display: inline-block;
+            margin: 0;
+            height: 20px;
+        }
+
+        .tanggal {
+            width: 100%;
+            text-align: end;
+        }
+    </style>
+<body>
+    <h2 style="text-align:center; margin: 0">Laporan Pembayaran Peternak</h2>
+    <h3 style="text-align:center; margin: 0">PT. Mitra Bhakti Makmur</h3>
+    <hr>
+    <h4 class="tanggal">Tanggal : <?= date('d-m-Y') ?></h4>
+
+    <section>
+        <table style="width:100%" border="1" cellspacing="0">
+            <tr>
+                <th>No</th>
+                <th>Nama petugas</th>
+                <th>Nama peternak</th>
+                <th>tanggal pembayaran</th>
+                <th>Periode</th>
+                <th>Total yang dibayarkan</th>
+            </tr>
+            <?php
+            if (isset($_GET['date_start']) && isset($_GET['date_end']) && isset($_GET['periode'])) {
+                $sql = "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran BETWEEN '" . $_GET['date_start'] . "' and '" . $_GET['date_end'] . "' AND periode = '" . $_GET['periode'] . "'";
+                $data = mysqli_query($conn, $sql);
+            } else {
+                //default tanpa filter
+                $sql = "SELECT * FROM pembayaran JOIN petugas ON pembayaran.id_petugas_transaksi = petugas.id_petugas JOIN peternak ON pembayaran.id_peternak = peternak.id_peternak WHERE tanggal_pembayaran";
+                $data = mysqli_query($conn, $sql);
+            }
+
+            $nomor = 1;
+            ?>
+            <?php while ($dataPembayaran = mysqli_fetch_array($data)) : ?>
+                <tr>
+                    <td><?= $nomor++ ?></td>
+                    <td><?= $dataPembayaran['nama'] ?></td>
+                    <td><?= $dataPembayaran['nama_pemilik'] ?></td>
+                    <td style="text-align:center"><?= $dataPembayaran['tanggal_pembayaran'] ?></td>
+                    <td style="text-align:center"><?= $dataPembayaran['periode'] ?></td>
+                    <td>Rp. <?= $dataPembayaran['harga_total'] ?></td>
+                </tr>
+            <?php endwhile ?>
+        </table>
+    </section>
+    <br>
+
+    <br>
+
+    
+    <div id="signature">
+        <div class="details">
+            <div class="identity">
+                <p>Malang, <?= date('d-m-Y') ?></p>
+                <p>Admin</p>
+            </div>
+            <p>Agus subarjo</p>
+        </div>
+    </div>
+</body>
+
+</html>
+<script>print()</script>
